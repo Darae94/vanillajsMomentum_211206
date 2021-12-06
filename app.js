8c169a38b683b69743b0ba83530cb5c064a6c62d
Darae94 <63302019+Darae94@users.noreply.github.com>
2021-12-06 13:54:11 +0900

    자바스크립트에서 html 읽기 및 변경
    자바스크립트에서 html 읽기 및 변경

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-// number, string
-// boolean, null(빈값), undefined(값이 없는것)
-// array(배열[]), object({키: 값}player.name, player["point"]..)
-// const -> constant 상수, 값 변경 불가 => 절대 바뀌면 안되는 변수에 적용!!(실수 예방)
-// var(과거 존재), let -> 값 변경 가능
-// function FunctionName(argument = 인수)
-// 인수는 해당 펑션 안에서만 사용 가능
-// 인수 넘기지 않고 실행하면 undefined, undefined를 연산하면 NaN
-// FunctionName() 따로 실행 하지 않으면 작동 없음
-// if, else if, else
-// isNaN(), ||, &&, ==, >, <, >=, >=, ===, true, false ... 
-// prompt(msg)
+document.title = "Hello! From JS!"
