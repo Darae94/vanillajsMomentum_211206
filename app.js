6bca5c4bcc070b7098a0cb322602a2ce54c860cc
Darae94 <63302019+Darae94@users.noreply.github.com>
2021-12-06 14:53:44 +0900

    calculator(+, -, *, /, **)

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-document.title = "Hello! From JS!"
+const calculator = {
+    plus: function(a, b) {
+        console.log(a + b);
+    },
+    minus: function(a, b) {
+        console.log(a - b);
+    },
+    times: function(a, b) {
+        console.log(a * b);
+    },
+    divide: function(a, b) {
+        console.log(a / b);
+    },
+    power: function(a, b) {
+        console.log(a ** b);
+    },
+}
+
+calculator.plus(8, 5);
+calculator.minus(8, 5);
+calculator.times(8, 5);
+calculator.divide(8, 5);
+calculator.power(8, 5);
