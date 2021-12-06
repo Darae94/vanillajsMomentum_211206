3754af102902d495898acc349888aa264682c27e
Darae94 <63302019+Darae94@users.noreply.github.com>
2021-12-06 13:36:26 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// number, string
+// boolean, null(빈값), undefined(값이 없는것)
+// array(배열[]), object({키: 값}player.name, player["point"]..)
+// const -> constant 상수, 값 변경 불가 => 절대 바뀌면 안되는 변수에 적용!!(실수 예방)
+// var(과거 존재), let -> 값 변경 가능
+// function FunctionName(argument = 인수)
+// 인수 넘기지 않고 실행하면 undefined, undefined를 연산하면 NaN
+// FunctionName() 따로 실행 하지 않으면 작동 없음
