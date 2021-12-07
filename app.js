abed0320f48b408d9abe8b3d598f00fb5920ce90
Darae94 <63302019+Darae94@users.noreply.github.com>
2021-12-07 09:53:59 +0900

    querySelector 사용해서 객체 찾아서 내용 변경

--- app.js
+++ app.js
@@ -1,33 +1,6 @@
-// typeof 해당 데이터의 타입 확인
-// parseInt => 스트링을 숫자로 변환
-// isNaN => 숫자가 아닌지 확인해서 true, false return!
-const age = parseInt(prompt("How old are you?"));
+// getElementsByClassName("hello") > 배열로 모든 클래스를 가져옴
+// querySelector(".hello h1") > 해당 조건의 첫번째 태그를 가져옴
+// querySelectorAll(".hello h1") > 배열로 해당 태그 정보를 가져옴
+const title = document.querySelector(".hello h1");
 
-// OR
-// true || true === true
-// true || false === true
-// false || true === true
-// false || false === false
-// AND
-// true && true === true
-// true && false === false
-// false && true === false
-// false && false === false
-
-if(isNaN(age) || age < 0) {
-    console.log("Please write a real positive number.");
-} else if(age < 18) {
-    console.log("You are too young.");
-} else if(age >= 18 && age <= 50) {
-    console.log("You can drink.");
-} else if(age > 50 && age <= 80) {
-    console.log("You should exercise!");
-} else if(age == 100) {
-    console.log("Wow you are wise")
-} else if(age > 80) {
-    console.log("You can do whatever you want.");
-}
-
-if((a && b) || (c && d) || (x || w)) {
-    
-}
+title.innerText = "hello!";
