b1935eed19ecb59848c09af6ca8244e59738fae9
Darae94 <63302019+Darae94@users.noreply.github.com>
2021-12-07 16:44:26 +0900

    window 관련 이벤트 실행

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // getElementsByClassName("hello") > 배열로 모든 클래스를 가져옴
 // querySelector(".hello h1") > 해당 조건의 첫번째 태그를 가져옴
 // querySelectorAll(".hello h1") > 배열로 해당 태그 정보를 가져옴
-const title = document.querySelector(".hello h1");
+const h1 = document.querySelector(".hello h1");
 
 function handleTitleClick() {
     this.style.color = "blue";
@@ -15,6 +15,27 @@ function handleMouseLeave() {
     this.innerText = "Mouse is gone!";
 }
 
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
+function handleWindowResize() {
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+    alert("copier!");
+}
+
+function handelWindowOffline() {
+    alert("SOS no WIFI");
+}
+
+function handelWindowOnline() {
+    alert("ALL GOOOD");
+}
+
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handelWindowOffline);
+window.addEventListener("online", handelWindowOnline);
