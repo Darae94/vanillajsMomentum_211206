5973266b6913f653a2befe64f2e59fd89c75d003
Darae94 <63302019+Darae94@users.noreply.github.com>
2021-12-08 15:57:41 +0900

    event.preventDefault() 사용해서 기본 동작 제어

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-// form submit 새로고침이 기본 동작임
+// form submit 새로고침(기본 동작)
+// a href 해당 주소로 이동(기본 동작)
 // 브라우저가 이벤트발생 시 onLoginSubmit(info) 실행 - info 는 이벤트 관련 정보
-// 
+// function({information about the event that just happened})
+// => 방금 일어난 이벤트에 대한 정보 제공
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const link = document.querySelector("a");
 
-function onLoginSubmit(tomato) {
-    tomato.preventDefault();
+function onLoginSubmit(event) {
+    event.preventDefault();
     console.log(loginInput.value);
 }
 
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.dir(event);
+    console.log(event);
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
