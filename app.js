79f9a45fddc1c87b7fa1fc2678064a1437a9f190
Darae94 <63302019+Darae94@users.noreply.github.com>
2021-12-08 14:56:21 +0900

    클릭했을 때 클래스 변화로 CSS 적용하기

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-// getElementsByClassName("hello") > 배열로 모든 클래스를 가져옴
-// querySelector(".hello h1") > 해당 조건의 첫번째 태그를 가져옴
-// querySelectorAll(".hello h1") > 배열로 해당 태그 정보를 가져옴
 const h1 = document.querySelector(".hello h1");
 
 function handleTitleClick() {
-    const currentColor = this.style.color;
-    let newColor;
-    if(currentColor === "blue") {
-        newColor = "tomato";
+    const clickedClass = "clicked"
+    if(h1.className === clickedClass) {
+        h1.className = "";
     } else {
-        newColor = "blue";
+        h1.className = clickedClass;
     }
-    this.style.color = newColor;
 }
+
 h1.addEventListener("click", handleTitleClick);
