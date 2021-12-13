e493304e0b9076f1f2c98bdb11e8b973560d97ca
Darae94 <63302019+Darae94@users.noreply.github.com>
2021-12-13 09:43:22 +0900

    localStorage 사용

--- app.js
+++ app.js
@@ -2,16 +2,17 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
-const HIDDEN_CLASSNAME = "hidden";
+const HIDDEN_CLASSNAME = "hidden"; // 보여주고 숨겨줄 클래스명
 
 function onLoginSubmit(event) {
-    event.preventDefault();
-    loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;
-    // 새로운 방식 : `` 안에 ${변수명}을 사용하면 기존에 문자열 합치는 것과 같은 역할을 함
-    // `Hello ${username}` === "Hello " + username;
-    greeting.innerHTML = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    event.preventDefault(); // 기본 동작 멈추기
+    loginForm.classList.add(HIDDEN_CLASSNAME); // 입력폼을 가리기 위해 클래스 추가
+    const username = loginInput.value; // 입력받은 값을 변수로 저장
+    // 유저명 기억 필요! - localStorage : mini DB 같은 역할
+    // setItem, getItem, removeItem
+    localStorage.setItem("username", username);
+    greeting.innerHTML = `Hello ${username}`; // 변수를 이용해 문구 추가
+    greeting.classList.remove(HIDDEN_CLASSNAME); // 보여주기 위해 클래스 삭제
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
