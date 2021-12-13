3b036b8cf006593ea0d58aeaf6cf7b042140a67f
Darae94 <63302019+Darae94@users.noreply.github.com>
2021-12-13 10:10:56 +0900

    #4 greeting End!!!

--- app.js
+++ app.js
@@ -2,25 +2,21 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
-const HIDDEN_CLASSNAME = "hidden"; // 보여주고 숨겨줄 클래스명
+const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
-    event.preventDefault(); // 기본 동작 멈추기
-    loginForm.classList.add(HIDDEN_CLASSNAME); // 입력폼을 가리기 위해 클래스 추가
-    const username = loginInput.value; // 입력받은 값을 변수로 저장
-    // 유저명 기억 필요! - localStorage : mini DB 같은 역할
-    // setItem, getItem, removeItem
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username);
     paintGreetings(username);
 }
 
 function paintGreetings(username) {
-    greeting.innerHTML = `Hello ${username}`; // 변수를 이용해 문구 추가
-    greeting.classList.remove(HIDDEN_CLASSNAME); // 보여주기 위해 클래스 삭제
+    greeting.innerHTML = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
-
-loginForm.addEventListener("submit", onLoginSubmit);
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
