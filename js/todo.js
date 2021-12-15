173c2d9e5d53e709bfa51be717fcf3eb8fd146c1
Darae94 <63302019+Darae94@users.noreply.github.com>
2021-12-15 15:18:58 +0900

    로컬스토리지의 데이터 형식 변경 후 각각의 id 값 주기

--- js/todo.js
+++ js/todo.js
@@ -12,13 +12,15 @@ function saveToDos() {
 
 function deleteToDo(event) {
     const li = event.target.parentElement;
+    console.log(li.id);
     li.remove();
 }
 
 function paintToDo(newTodo) {
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
-    span.innerHTML = newTodo;
+    span.innerHTML = newTodo.text;
     const button = document.createElement("button");
     button.innerHTML = "❌";
     button.addEventListener("click", deleteToDo);
@@ -31,8 +33,12 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(),
+    }
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     saveToDos();
 }
 
