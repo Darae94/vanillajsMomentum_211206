fc08ac11d175a5eac42833640875d707e0a0e3f4
Darae94 <63302019+Darae94@users.noreply.github.com>
2021-12-15 17:25:30 +0900

    filter 사용법

--- js/todo.js
+++ js/todo.js
@@ -51,3 +51,14 @@ if(savedToDos !== null) { // todo 리스트 내용이 있으면
     toDos = parsedToDos; // 저장된값도 불러와서 사용
     parsedToDos.forEach(paintToDo); // 저장된 값 보여주기
 }
+
+function sexyFilter() {
+
+}
+
+// [1, 2, 3, 4].filter(sexyFilter)
+// return true의 경우 해당 값 유지
+// sexyFilter(1) = 1
+// sexyFilter(2) = 2
+// sexyFilter(3) x
+// sexyFilter(4) = 4
