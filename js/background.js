3945101ab856d485454dbc4211ff95804b4fccf4
Darae94 <63302019+Darae94@users.noreply.github.com>
2022-04-12 22:22:21 +0900

    electron 사용 앱 만들기
    modules 및 pakage.json 안올림

--- js/background.js
+++ js/background.js
@@ -1,7 +1,9 @@
-const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg"];
+function getBackground() {
+    const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg"];
+    const chosenImage = images[Math.floor(Math.random() * images.length)];
+    const bgImage = `url(img/${chosenImage})`;
+    
+    document.body.style.backgroundImage = bgImage;
+}
 
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-const bgImage = `url(img/${chosenImage})`;
-
-document.body.style.backgroundImage = bgImage;
+getBackground();
